b30cc15ef7ed51c90cd245d7d06596760a0d67f6
mattbesedick <mattbezy@gmail.com>
2020-11-30 16:32:02 -0600

    initial commit'

---
+++ api/firebase.js
@@ -0,0 +1,6 @@
+import firebase from "firebase/app";
+import firebaseConfig from './firebaseConfig'
+// Add the Firebase services that you want to use
+import "firebase/firestore";
+
+firebase.initializeApp(firebaseConfig);
